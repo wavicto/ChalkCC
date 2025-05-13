b39dc1c93750d44da05fcaddb990bd10c98e9141
victow <victorwang221@gmail.com>
2025-05-13 19:45:08 -0400

    Base lexer complete

--- lexer.cpp
+++ lexer.cpp
@@ -11,11 +11,61 @@ std::regex lexer::open_b("\\{");
 std::regex lexer::close_b("\\}");
 std::regex lexer::semicol(";");
 
+//malformed token check
+std::regex lexer::malformed_token("[0-9]+[a-zA-Z_]\\w*\\b");
+
 lexer::lexer(){
 
 }
 
+std::vector<Token> lexer::extract(std::ifstream &file){
+    std::string section;
+    std::vector<Token> list;
+
+    while(file >> section){
+        try {
+        tokenize(list, section);
+        } catch (const std::runtime_error& e) {
+            std::cerr << "Lexer error: " << e.what() << std::endl;
+            std::exit(1);
+        }
+    }
+    return list;
+}
+
+void lexer::tokenize(std::vector<Token> &list, std::string token){
+    int start = 0;
+    int end = 1;
+
+    while (token.length() != 0){
+        std::string subtoken = token.substr(start, end);
+        while(!matches(subtoken)){
+            end++;
+            subtoken = token.substr(start, end);
+            if (end == token.length() + 1){
+                throw std::runtime_error("Invalid Token");
+            }
+        }
+        while(matches(subtoken)){
+            if (end == token.length()+1){
+                break;
+            }
+            end++;
+            subtoken = token.substr(start, end);
+        }
+        end--;
+        token_adder(list, token.substr(start, end));
+        token = token.substr(end, token.length() - end);
+        end = 1;
+    }
+    
+}
+
 bool lexer::matches(const std::string& token) {
+    if (std::regex_match(token, malformed_token)){
+        throw std::runtime_error("Malformed Token");
+    }
+
     bool result = std::regex_match(token, open_b) ||
     std::regex_match(token, close_b) ||
     std::regex_match(token, semicol) ||
@@ -60,42 +110,4 @@ void lexer::token_adder(std::vector<Token> &list, std::string token){
     else if (std::regex_match(token, close_p)) {
         list.push_back(Token(Close_parenthesis));
     }
-}
-
-void lexer::tokenize(std::vector<Token> &list, std::string token){
-    int start = 0;
-    int end = 1;
-
-    while (token.length() != 0){
-        std::string subtoken = token.substr(start, end);
-        while(!matches(subtoken)){
-            end++;
-            subtoken = token.substr(start, end);
-            if (end == token.length() + 1){
-                throw std::runtime_error("Lexer: potential source code syntax error");
-            }
-        }
-        while(matches(subtoken)){
-            if (end == token.length()+1){
-                break;
-            }
-            end++;
-            subtoken = token.substr(start, end);
-        }
-        end--;
-        token_adder(list, token.substr(start, end));
-        token = token.substr(end, token.length() - end);
-        end = 1;
-    }
-    
-}
-
-std::vector<Token> lexer::extract(std::ifstream &file){
-    std::string section;
-    std::vector<Token> list;
-
-    while(file >> section){
-        tokenize(list, section);
-    }
-    return list;
 }
